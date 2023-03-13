f800d2fb2d37f77772505f8ac4d955cf56f9f5f1
leonardo <l_b384@txstate.edu>
2023-03-13 04:34:32 -0500

    node.js server setup & basic express API endpoints

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  console.log(req.params);
+  res.json(note);
+});
+
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
