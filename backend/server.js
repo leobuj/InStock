923bd01e3e3faaee4de289a8d0080047a3beb594
Sean Angle <sla88@txstate.edu>
2023-03-30 18:11:16 -0500

    changed port number in server file

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 dotenv.config();
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
   res.send("API is running...");
