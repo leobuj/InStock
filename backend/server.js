6b8c1f4ff38a23df9b6fd77a39654e55adfbb9af
leonardo <l_b384@txstate.edu>
2023-03-30 21:06:33 -0500

    Changed Port From 5000 to 5001

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 connectDB();
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
   res.send("API is running...");
