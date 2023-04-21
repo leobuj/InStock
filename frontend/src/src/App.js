9676dab90179a89134f40aae43aa4006dd313f0d
ZacharyBenavides <127966332+ZacharyBenavides@users.noreply.github.com>
2023-04-21 08:27:17 -0500

    Functioning Login
    Cleaned up the visuals a tad, login now sends information, and register now correctly displays.

---
+++ frontend/src/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Header from "./components/Header/Header"
+import Footer from './components/Footer/Footer';
+import LandingPage from './screens/LandingPage/LandingPage';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import TestStock from './screens/TestStock/TestStock';
+import Login from './screens/RegisLog/Login';
+import Register from './screens/RegisLog/Register';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Routes>
+          <Route path='/' element={<LandingPage />} exact/>
+          <Route path='/teststock' element={<TestStock />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/register' element={<Register />}/>
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
