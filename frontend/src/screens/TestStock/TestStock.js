df499094d64f440c97c4fb7b84afad5a3727e5dd
enc-code <70240485+enc-code@users.noreply.github.com>
2023-04-23 18:24:57 -0500

    Update TestStock.js

--- frontend/src/screens/TestStock/TestStock.js
+++ frontend/src/screens/TestStock/TestStock.js
@@ -1,50 +1,82 @@
-//import { useEffect, useState } from "react";
-import {Button, Card} from "react-bootstrap";
+import { useEffect, useState } from "react";
+import {Button, Card, Badge, Accordion} from "react-bootstrap";
 import {Link} from "react-router-dom";
-import MainScreen from "../../components/MainScreen";
-//import axios from "axios";
+import MainStock from "../../components/MainStock";
+import axios from "axios";
 
 function TestStock() {
-    /*const [stock, setStock] = useState([]);
+    const deleteHandler = (id) => {
+        if (window.confirm("Certain?")){
+
+        }
+    };
+    
+    const [stock, setStock] = useState([]);
     const fetchStock = async() => {
-        const { data } = await axios.get('/api/notes')
+        const { data } = await axios.get('/api/items')
     
         setStock(data);
     };
     
     console.log(stock);
     
-    useEffect(() => {fetchStock();}, []);*/
-    
+    useEffect(() => {fetchStock();}, []);
     return(
-        <MainScreen title="Welcome back dear user!"> Testing My Stock
+        <MainStock title="Inventory"> Inventory
             <Link to='addstock'>
                 <Button style={{marginLeft: 10, marginBottom: 6}} size='lg'>
                     Add new/existing stock
                 </Button>
-                    <Card style={{margin: 10}}>
-                        <Card.Header style={{display: "flex"}}>
-                            <span
-                            style={{
-                                color: "maroon",
-                                textDecoration: "none",
-                                flex: 1,
-                                cursor: "pointer",
-                                alignSelf: "center",
-                                fontSize: 18
-                            }}
-                            >title</span>
-                        </Card.Header>
-                        <div>
-                            <Button>Edit</Button>
-                            <Button variant="danger" className="mx-2">
-                                Delete
-                            </Button>
-                        </div>
-                    </Card>
             </Link> 
-        </MainScreen>
-
+                {
+                    stock.map(note => (
+                        <Accordion key={note._id}>
+                            <Accordion.Item eventKey="0">
+                            <Card style={{margin: 10}}>
+                            <Card.Header style={{display: "flex"}}>
+                                <span
+                                style={{
+                                    color: "maroon",
+                                    textDecoration: "none",
+                                    flex: 1,
+                                    cursor: "pointer",
+                                    alignSelf: "center",
+                                    fontSize: 18
+                                }}
+                                ><Accordion.Button as={Card.Text} bg="link" eventKey="0">{stock.title}</Accordion.Button>
+                                </span>
+                            </Card.Header>
+                            <div>
+                                <Button href={`/note/${note._id}`}>Edit</Button>
+                                <Button variant="danger" className="mx-2" onClick={() => deleteHandler(note._id)}>
+                                    Delete
+                                </Button>
+                            </div>
+                            <Accordion.Collapse eventKey="0">
+                            <Card.Body>
+                                <h4>
+                                    <Badge variant='success'>
+                                        Category - {note.category}
+                                    </Badge>
+                                </h4>
+                                <blockquote className="blockquote mb-0">
+                                    <p>
+                                        {note.content}
+                                    </p>
+                                    <footer className="blockquote-footer">
+                                        Added on - date
+                                    </footer>
+                                </blockquote>
+                            </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                            </Accordion.Item>
+                        </Accordion>
+                        
+                    ))}
+        </MainStock>
+        
+        
     );
 };
 
